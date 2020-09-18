fbcb49b76e8b5a05c5fc7ae246332dfadab745b2
Benedikt Beckermann <hello@benedikt.click>
2020-09-18 15:29:04 +0200

    added recursive version of binary search

--- kata_2020-09-18_karate-chop/binary_chop.py
+++ kata_2020-09-18_karate-chop/binary_chop.py
@@ -17,3 +17,20 @@ def chop_while(target, search_array):
             left_border = lookup_index+1
 
     return -1
+
+def chop_rec(target, search_array):
+    return chop_rec_calc(target, search_array, 0, len(search_array))
+
+def chop_rec_calc(target, search_array, left, right):
+    if left >= right:
+        return -1
+
+    lookup_index = int((left+right)/2)
+    lookup_result = search_array[lookup_index]
+
+    if lookup_result == target:
+        return lookup_index
+    elif lookup_result > target:
+        return chop_rec_calc(target, search_array, left, lookup_index)
+    else:
+         return chop_rec_calc(target, search_array, lookup_index+1, right)
